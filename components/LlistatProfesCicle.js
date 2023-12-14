71f0bc2722de9fdd4fea6a20b5200a6b96d0d55b
Alberto Villegas <alvich@floridauniversitaria.es>
2023-12-14 15:00:01 +0100

    Acabando switch

--- components/LlistatProfesCicle.js
+++ components/LlistatProfesCicle.js
@@ -11,16 +11,13 @@ const LlistatProfesCicle = props => {
 
   const onToggleSwitch = () => {
     setIsSwitchOn(!isSwitchOn)
-    if (!isSwitchOn) {
-      setIndex(1);
-    } else {
-      setIndex(0);
-    }
+    let selectedCursIndex = isCicleDAM ? isSwitchOn ? 1 : 0 : 0;
+    setIndex(selectedCursIndex);
   };
 
   return (
     <View style={{ flex: 2.5, padding: 3, borderColor: 'red' }}>
-      {isCicleDAM && props.cicleSeleccionat !== null ? (
+      {props.cicleSeleccionat !== null ? (
         props.dades.unitatTics[props.cicleSeleccionat].curs[index].profes.map(
           (unModul, index) => {
             return (
@@ -38,16 +35,16 @@ const LlistatProfesCicle = props => {
         </Text>
       )}
       {isCicleDAM && (
-          <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 15, fontWeight: '900', marginRight: 5 }}>1r</Text>
-            <Switch
-              color="red"
-              onValueChange={onToggleSwitch}
-              value={isSwitchOn}
-            />
-            <Text style={{ fontSize: 15, fontWeight: '900', marginRight: 5 }}>2n</Text>
-          </View>
-        )}
+        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ fontSize: 15, fontWeight: '900', marginRight: 5 }}>1r</Text>
+          <Switch
+            color="red"
+            onValueChange={onToggleSwitch}
+            value={isSwitchOn}
+          />
+          <Text style={{ fontSize: 15, fontWeight: '900', marginRight: 5 }}>2n</Text>
+        </View>
+      )}
     </View>
   );
 };

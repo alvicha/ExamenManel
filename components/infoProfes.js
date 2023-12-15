68d5f1842b0949d0328931f1f757bac680d52828
Alberto Villegas <alvich@floridauniversitaria.es>
2023-12-15 01:00:58 +0100

    Fent boto Eliminar professor de la llista

--- components/infoProfes.js
+++ components/infoProfes.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { Avatar, Chip, Card, Provider as PaperProvider } from 'react-native-paper';
+import { Avatar, Portal, Dialog, Chip, Button, Card, Provider as PaperProvider } from 'react-native-paper';
 
 const InfoProfes = (props) => {
   const arrayColores = ['#FDE5E5', '#D1F2D1', '#D1F2F0', '#FAF9BA', '#E7D1F2'];
   const [colorFondoIndex, setColorFondoIndex] = useState(0);
   const [mostrarChipProfessor, setMostrarChipProfessor] = useState(false);
+  const [visible, setVisible] = React.useState(false);
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
+
   const donarChipActivat = () => {
     setMostrarChipProfessor(!mostrarChipProfessor);
     setColorFondoIndex(props.index);
@@ -15,36 +19,82 @@ const InfoProfes = (props) => {
     return (<Avatar.Icon size={40} icon={props.dades.foto} />)
   }
 
+  const eliminarProfessorLlista = (index) => {
+    
+    const cicleIndex = props.dades.unitatTics.findIndex(c => c.cicle === cicleToRemove);
+    
+    if (cicleIndex !== -1) {
+      // Encuentra el índice del curs dentro del cicle
+      const cursIndex = props.dades.unitatTics[cicleIndex].curs.findIndex(c => c.numCurs === numCursToRemove);
+    
+      if (cursIndex !== -1) {
+        // Encuentra el índice del profesor dentro del curs
+        const profesIndex = miJson.unitatTics[cicleIndex].curs[cursIndex].profes.findIndex(p => p.nom === nomProfToRemove);
+    
+        if (profesIndex !== -1) {
+          // Elimina el profesor
+          miJson.unitatTics[cicleIndex].curs[cursIndex].profes.splice(profesIndex, 1);
+        }
+      } 
+    }
+
+    setVisible(false);
+
+  }
+
   return (
     <ScrollView contentContainerStyle={styles.contentLlistatProfes}>
-      <View style={styles.chip}>
-        <Chip icon="human-male-board" onPress={donarChipActivat}>
-          {props.dades.nom}
-        </Chip>
-        {mostrarChipProfessor && (
-          <Card mode='outlined' style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-start',
-            alignItems: 'flex-end',
-            backgroundColor: arrayColores[colorFondoIndex],
-          }}>
-            <Card.Title titleStyle={styles.title}
-              title={props.dades.nom} right={fotoProfessor} />
-            <Card.Content>
-              <Text style={styles.modul} variant="bodyMedium">Modul:
-                <Text style={styles.textModul}>{props.dades.modul}</Text>
-              </Text>
-              <Text style={styles.enunciatCompletModul} variant="bodyMedium">Nom complet modul:
-                <Text style={styles.completModul}>{props.dades.nomCompletModul}</Text>
-              </Text>
-              <Text style={styles.enunciatHoresSetmanals} variant="bodyMedium">Hores setmanals:
-                <Text style={styles.horesSetmanals}>{props.dades.horesSetmanals}</Text>
-              </Text>
-            </Card.Content>
-          </Card>
-        )
-        }
-      </View>
+      <PaperProvider>
+        <View style={styles.chip}>
+          <Chip icon="human-male-board" onPress={donarChipActivat}>
+            {props.dades.nom}
+          </Chip>
+          {mostrarChipProfessor && (
+            <Card mode='outlined' style={{
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              alignItems: 'flex-end',
+              backgroundColor: arrayColores[colorFondoIndex],
+            }}>
+              <Card.Title titleStyle={styles.title}
+                title={props.dades.nom} right={fotoProfessor} />
+              <Card.Content style={styles.cardContent}>
+                <Text style={styles.modul} variant="bodyMedium">Modul:
+                  <Text style={styles.textModul}>{props.dades.modul}</Text>
+                </Text>
+                <Text style={styles.enunciatCompletModul} variant="bodyMedium">Nom complet modul:
+                  <Text style={styles.completModul}>{props.dades.nomCompletModul}</Text>
+                </Text>
+                <Text style={styles.enunciatHoresSetmanals} variant="bodyMedium">Hores setmanals:
+                  <Text style={styles.horesSetmanals}>{props.dades.horesSetmanals}</Text>
+                </Text>
+              </Card.Content>
+              <View style={{ justifyContent: 'flex-end', alignItems: 'flex-end' }}>
+                <Button
+                  textColor='green'
+                  icon="delete"
+                  mode="text"
+                  onPress={showDialog}
+                  style={{ borderRadius: 0 }}
+                />
+              </View>
+            </Card>
+          )
+          }
+        </View>
+        <Portal>
+          <Dialog visible={visible} onDismiss={hideDialog}>
+            <Dialog.Title>Eliminacio Professor</Dialog.Title>
+            <Dialog.Content>
+              <Text variant="bodyMedium">Estàs segur que vols esborrar a aquest professor de la llista?</Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={hideDialog}>No</Button>
+              <Button onPress={() => eliminarProfessorLlista(props.index)}>Si</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
+      </PaperProvider>
     </ScrollView>
   );
 };
@@ -53,6 +103,11 @@ const styles = StyleSheet.create({
   contentLlistatProfes: {
     flexDirection: 'column',
     justifyContent: 'space-around',
+  },
+  cardContent: {
+    flex: 1,
+    width: 350,
+    justifyContent: 'space-between',
   },
   chip: {
     padding: 10,
@@ -113,12 +168,7 @@ const styles = StyleSheet.create({
     fontWeight: '900',
     textAlign: 'left',
     color: 'darkorange'
-  },
-  card: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end'
-  },
+  }
 });
 
 export default InfoProfes;

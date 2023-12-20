8a2ed06dd7203da647430948fcc0e77709f6a3eb
Alberto Villegas <alvich@floridauniversitaria.es>
2023-12-20 20:55:36 +0100

    Fent canvis amb Modal

---
+++ components/ModalInfoProfes.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Switch, TextInput, Portal, Modal } from 'react-native-paper';
+
+
+const ModalInfoProfes = props => {
+    const [modalVisible, setModalVisible] = React.useState(true);
+    const hideModal = () => setModalVisible(false);
+    const showModal = () => setModalVisible(true);
+    const containerStyle = { backgroundColor: 'white', padding: 120 };
+
+    return (
+        <Portal>
+            <Modal visible={modalVisible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                <View style={{ flex: 1, borderColor: 'red', borderWidth: 3 }}>
+                    <Text>Editació Profes</Text>
+                    <View style={styles.contentDades}>
+                        <View style={styles.textInputPeso}>
+                            <TextInput
+                                style={styles.resultat}
+                                label="Nom professor"
+                                //onChangeText={(text) => handlePesChange(text)}
+                                value={props.dades.nom}
+                                mode='outlined'
+                                outlineColor='green'
+                            />
+                        </View>
+                        <View style={styles.textInputAltura}>
+                            <TextInput
+                                style={styles.resultat}
+                                value={props.dades.modul}
+                                label="Modul"
+                                //onChangeText={(text) => handleAlçadaChange(text)}
+                                mode='outlined'
+                                outlineColor='orange'
+                            />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+        </Portal>
+    );
+};
+
+const styles = StyleSheet.create({
+    contentLlistatProfes: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    textInputPeso: {
+        textAlign: 'left',
+        width: 160,
+    },
+    textInputAltura: {
+        justifyContent: 'flex-start',
+        width: 160,
+    },
+    contentDades: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        padding: 9,
+    }
+});
+
+export default ModalInfoProfes;
--- components/infoProfes.js
+++ components/infoProfes.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { Avatar, Portal, Dialog, Chip, Button, Card, Provider as PaperProvider } from 'react-native-paper';
+import { Avatar, Portal, Dialog, Chip, Button, Card, PaperProvider, Modal, TouchableRipple } from 'react-native-paper';
+import ModalInfoProfes from './ModalInfoProfes';
 
 const InfoProfes = (props) => {
   const arrayColores = ['#FDE5E5', '#D1F2D1', '#D1F2F0', '#FAF9BA', '#E7D1F2'];
   const [colorFondoIndex, setColorFondoIndex] = useState(0);
   const [mostrarChipProfessor, setMostrarChipProfessor] = useState(false);
   const [visible, setVisible] = React.useState(false);
+  const [modalVisible, setModalVisible] = React.useState(false);
+
   const showDialog = () => setVisible(true);
   const hideDialog = () => setVisible(false);
+
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
 
   const donarChipActivat = () => {
     setMostrarChipProfessor(!mostrarChipProfessor);
@@ -19,62 +25,42 @@ const InfoProfes = (props) => {
     return (<Avatar.Icon size={40} icon={props.dades.foto} />)
   }
 
-  const eliminarProfessorLlista = () => {
-    props.eliminaProfessorLlista(props.index);
-    setVisible(false);
-  }
-
   return (
-      <PaperProvider>
-        <View style={styles.chip}>
-          <Chip icon="human-male-board" onPress={donarChipActivat}>
-            {props.dades.nom}
-          </Chip>
-          {mostrarChipProfessor && (
-            <Card mode='outlined' style={{
-              flexDirection: 'row',
-              justifyContent: 'flex-start',
-              alignItems: 'flex-end',
-              backgroundColor: arrayColores[colorFondoIndex],
-            }}>
-              <Card.Title titleStyle={styles.title}
-                title={props.dades.nom} right={fotoProfessor} />
-              <Card.Content style={styles.cardContent}>
-                <Text style={styles.modul} variant="bodyMedium">Modul:
-                  <Text style={styles.textModul}>{props.dades.modul}</Text>
-                </Text>
-                <Text style={styles.enunciatCompletModul} variant="bodyMedium">Nom complet modul:
-                  <Text style={styles.completModul}>{props.dades.nomCompletModul}</Text>
-                </Text>
-                <Text style={styles.enunciatHoresSetmanals} variant="bodyMedium">Hores setmanals:
-                  <Text style={styles.horesSetmanals}>{props.dades.horesSetmanals}</Text>
-                </Text>
-              </Card.Content>
-              <View style={styles.botoPapelera}>
-                <Button
-                  textColor='green'
-                  icon="delete"
-                  mode="text"
-                  onPress={showDialog}
-                />
-              </View>
-            </Card>
-          )
-          }
-        </View>
-        <Portal>
-          <Dialog visible={visible} onDismiss={hideDialog}>
-            <Dialog.Title>Eliminacio Professor</Dialog.Title>
-            <Dialog.Content>
-              <Text variant="bodyMedium">Estàs segur que vols esborrar al professor {props.dades.nom} amb modul {props.dades.modul} de la llista?</Text>
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button onPress={hideDialog}>No</Button>
-              <Button onPress={eliminarProfessorLlista}>Si</Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
-      </PaperProvider>
+    <PaperProvider>
+      {modalVisible ?
+        (<ModalInfoProfes dades={props.dades} />) :
+        (
+          <View style={styles.chip}>
+            <Chip icon="human-male-board" onPress={donarChipActivat}>
+              {props.dades.nom}
+            </Chip>
+            {mostrarChipProfessor && (
+              <Card mode='outlined' style={{
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-end',
+                backgroundColor: arrayColores[colorFondoIndex],
+              }}
+                onPress={showModal}>
+                <Card.Title titleStyle={styles.title}
+                  title={props.dades.nom} right={fotoProfessor} />
+                <Card.Content style={styles.cardContent}>
+                  <Text style={styles.modul} variant="bodyMedium">Modul:
+                    <Text style={styles.textModul}>{props.dades.modul}</Text>
+                  </Text>
+                  <Text style={styles.enunciatCompletModul} variant="bodyMedium">Nom complet modul:
+                    <Text style={styles.completModul}>{props.dades.nomCompletModul}</Text>
+                  </Text>
+                  <Text style={styles.enunciatHoresSetmanals} variant="bodyMedium">Hores setmanals:
+                    <Text style={styles.horesSetmanals}>{props.dades.horesSetmanals}</Text>
+                  </Text>
+                </Card.Content>
+              </Card>
+            )
+            }
+          </View>
+        )}
+    </PaperProvider >
   );
 };
 
